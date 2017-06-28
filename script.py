72706d5931baf31963a4dd7c606ea23f84ad4221
StasDeep <glubokiy.stas@gmail.com>
2017-06-28 16:27:12 +0300

    Add script.py with basic info.

---
+++ script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#------------------------------------------------------------------------------
+# Name:     Wikipedia Company URL Scraper
+# Purpose:  Easy URL retrieval from Wikipedia
+#
+# Created:  28th of June 2017
+# Author:   StasDeep
+#------------------------------------------------------------------------------
+
+"""Retrieves websites of companies from Wikipedia.
+
+Usage:
+  $ python script.py wikipedia_links.csv
+"""
